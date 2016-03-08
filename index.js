d897713e6b551474158824868719a4bd1332a5c6
msherron <msherron@gmail.com>
2016-03-08 12:18:32 -0800

    removing dummy data and adding mysql

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
+var mysql = require("mysql");
+
 var app = express()
 var port = process.env.PORT || 5000
+
+var con = mysql.createConnection({
+  host: "sherron.cq0y78xmuomp.us-east-1.rds.amazonaws.com",
+  database: "hackathon",
+  user: "admin",
+  password: "admin1234"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
 
 app.use(express.static(__dirname + "/"))
 
@@ -16,32 +33,17 @@ console.log("websocket server created")
 
 wss.on("connection", function(ws) {
   
-  var dummy_data = [
-    {
-      'nid': 182786,
-      'title': 'Home Again',
-      'thumb': '/sites/default/files/styles/full_episodes_rotator_thumbnail/public/video-image-thumb/fox_TheXFiles_1AYW02_hulu.jpg',
-      'type': 'video',
-      'url': '/watch/616557123886/7756658688' 
-    },
-    {
-      'nid': 187501,
-      'title': 'Mr. Freeze',
-      'thumb': '/sites/default/files/styles/full_episodes_rotator_thumbnail/public/video-image-thumb/Gotham_GTH212_2500_1280x720_633837635777.jpg',
-      'type': 'video',
-      'url': '/watch/633284675626/7684182528'
-    },
-    {
-      'nid': 188896,
-      'title': 'Wingman',
-      'thumb': '/sites/default/files/styles/full_episodes_rotator_thumbnail/public/video-image-thumb/fox_Lucifer_L106_hulu.jpg',
-      'type': 'video',
-      'url': '/watch/638578243933/7766097408'
-    }
-  ];
+  var query = "SELECT * FROM popular_content ORDER BY 'view_count' DESC limit 3";
+
+  con.query(query, function(err,rows){
+  	if (err) throw err;
+
+  	console.log('Data received from Db:\n');
+  	console.log(rows);
+  })
 
   var id = setInterval(function() {
-    ws.send(JSON.stringify(dummy_data))
+    ws.send(JSON.stringify(rows))
   }, 5000)
 
   console.log("websocket connection open")
