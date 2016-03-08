01b66b6735c8e7392921f52274d4a0bb62d2b0ee
msherron <msherron@gmail.com>
2016-03-08 15:30:17 -0800

    adding REST implementation and handling of POST data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var mysql = require("mysql");
 
-var app = express()
+// Setup Socket Server - so browser clients can connect and
+// listen for updates.
+var WebSocketServer = require("ws").Server
+
+// Dependencies for REST interface.
+var http = require("http")
+var connect = require("connect")
+var express = require("express")
+var app = connect()
+
+// Will parse POST body content.
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 var port = process.env.PORT || 5000
 
+// Set up MySQL connection.
+var mysql = require("mysql")
 var con = mysql.createConnection({
   host: "sherron.cq0y78xmuomp.us-east-1.rds.amazonaws.com",
   database: "hackathon",
@@ -21,6 +33,31 @@ con.connect(function(err){
   console.log('Connection established');
 });
 
+// Set REST endpoint to add page views from Drupal to our DB.
+app.use('/view_add', function (req, res, next) {
+
+  // Parse POST info.
+  var body = req.body;
+  var insert_data = {
+  	nid: body.nid, 
+  	title: body.title, 
+  	thumb: body.thumb,
+  	type: body.type,
+  	url: body.url,
+  	view_count: 1
+  }
+
+  // Check for an existing record and update, else insert.
+  var insert = "INSERT INTO popular_content SET ? ON DUPLICATE KEY UPDATE view_count = view_count + 1";
+  insert_query = con.query(insert, insert_data, function(err, result) {
+  	console.log(insert_query.sql);
+  	// Update if one exists.
+  })
+
+  res.end('Success!');
+});
+
+// Handle default route to ask for a socket connection (client browser).
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
@@ -31,6 +68,8 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+// When a connection is established, periodically send updates every X seconds
+// (currently 5).
 wss.on("connection", function(ws) {
   
   var id = setInterval(function() {
@@ -50,3 +89,4 @@ wss.on("connection", function(ws) {
     clearInterval(id)
   })
 })
+
